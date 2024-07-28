ed10aa55b931300504093f3799d8a29c38e325c0
Hansana Sandakelum <95872422+HansanaSandakelum@users.noreply.github.com>
2024-07-28 21:08:59 +0530

    Refactor store creation using @reduxjs/toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,22 +1,22 @@
-import { useSelector,useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 
-import { counterActions } from '../store/index';
-import classes from './Counter.module.css';
+import { counterActions } from "../store/counter";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const dispatch = useDispatch(); 
-  const counter = useSelector(state => state.counter.Counter);
-  const show = useSelector(state => state.counter.showCounter);
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
 
   const incrementHandler = () => {
     dispatch(counterActions.increment());
-  }
+  };
   const decrementHandler = () => {
     dispatch(counterActions.decrement());
-  }
+  };
   const increaseHandler = () => {
     dispatch(counterActions.increase(5));
-  }
+  };
   const toggleCounterHandler = () => {
     dispatch(counterActions.toggle());
   };
@@ -24,7 +24,7 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      {show&&<div className={classes.value}>{counter}</div>} 
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increment by 5</button>
